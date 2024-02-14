80aa5636af24ce32ce4721e07dbf5d66f08f5a4c
Ogunnusi Abayomi Joseph <70065792+drsimplegraffiti@users.noreply.github.com>
2024-02-14 13:22:53 +0100

    added email sending

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -10,23 +10,21 @@ import { LoginSchema, SignUpSchema } from "../schema/user";
 import { NotFoundException } from "../exceptions/not-found";
 import { sendEmail } from "../utils/emailsender";
 
-export const signup = async (
-  req: Request,
-  res: Response,
-) => {
-    SignUpSchema.parse(req.body);
-    const { email, name, password } = req.body;
-    let user = await prismaClient.user.findFirst({
-      where: {
-        email,
-      },
-    });
-    if (user) {
+export const signup = async (req: Request, res: Response) => {
+  SignUpSchema.parse(req.body);
+  const { email, name, password } = req.body;
+  let user = await prismaClient.user.findFirst({
+    where: {
+      email,
+    },
+  });
+  try {
+    if (user?.email === email) {
       // throw new Error("User already exists!");
-        new BadRequestsException(
-          "User already exists!",
-          ErrorCode.USER_ALREADY_EXISTS
-        )
+      new BadRequestsException(
+        "User already exists!",
+        ErrorCode.USER_ALREADY_EXISTS
+      );
     }
     user = await prismaClient.user.create({
       data: {
@@ -35,14 +33,20 @@ export const signup = async (
         password: hashSync(password, 10),
       },
     });
+  } catch (err) {
+    throw new BadRequestsException(
+      "User already exists!",
+      ErrorCode.USER_ALREADY_EXISTS
+    );
+  }
 
-    await sendEmail({
-      email: user.email,
-      subject: "Welcome to our app",
-      message: "Welcome to our app",
-    });
+  await sendEmail({
+    email: user.email,
+    subject: "Welcome to our app",
+    message: `Welcome to our app, ${user.name}, we are glad to have you!, you can now login to our app with your email and password.`,
+  });
 
-    return res.json(user);
+  return res.json(user);
 };
 
 export const login = async (req: Request, res: Response) => {
@@ -82,5 +86,3 @@ export const login = async (req: Request, res: Response) => {
 export const me = async (req: Request, res: Response) => {
   return res.json(req?.user);
 };
-
-
